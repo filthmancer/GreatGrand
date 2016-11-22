5f09d4b19321f2cf5be59782aac008e1db81f6ed
Matt Horton <maxerj@gmail.com>
2016-11-22 21:00:35 +1100

    0.02
    Working on face editor

---
+++ Assets/Editor/GrandGen.cs
@@ -0,0 +1,260 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GrandGen : EditorWindow {
+
+
+	[MenuItem("Custom/Grand Generator")]
+	static void Init()
+	{
+		GrandGen window = (GrandGen)EditorWindow.GetWindow(typeof(GrandGen));
+		window.Show();
+	}
+
+	static FaceObjInfoContainer Base;
+
+	static FaceObjInfoContainer Eye,
+								Nose,
+								Brow,
+								Hair,
+								Ear,
+								Jaw;
+
+	int tab = 0;
+
+	bool Eye_Pupils = false;
+	int Eye_Current  = 0,
+		Nose_Current  = 0,
+		Brow_Current  = 0,
+		Hair_Current  = 0,
+		Ear_Current  = 0,
+		Jaw_Current  = 0,
+		Base_Current = 0;
+
+	public FaceObjInfo 	Base_Final,
+						Eye_Final,
+						Nose_Final,
+						Hair_Final,
+						Brow_Final,
+						Ear_Final,
+						Jaw_Final;
+
+	public FaceObjInfoContainer FOA(int num)
+	{
+		switch(num)
+		{
+			//GRAND INFO
+			case 0:
+			return Base;
+			break;
+			//Base
+			case 1:
+			return Hair;
+			break;
+			//Jaw
+			case 2:
+			return Jaw;
+			break;
+			//Eye
+			case 3:
+			return Eye;
+			break;
+			//Nose
+			case 4:
+			return Nose;
+			break;
+			//Brow
+			case 5:
+			return Brow;
+			break;
+			//Ear
+			case 6:
+			return Ear;
+			break;
+		}
+		return Base;
+	}
+
+	void OnGUI()
+	{
+		EditorGUILayout.BeginHorizontal();
+		GUILayout.Label("Grand Generator", EditorStyles.boldLabel);
+		if(GUILayout.Button("Get Elements"))
+		{
+			LoadElements();
+		}
+		if(GUILayout.Button("Generate"))
+		{
+			Generate();
+		}
+		EditorGUILayout.EndHorizontal();
+		EditorGUILayout.Space();
+
+		if(!elements_loaded) return;
+		tab = GUILayout.Toolbar(tab, new string[] {"Base", "Hair","Jaw","Eye", 
+													"Nose", "Brow", "Ear"});
+		FaceObjInfoContainer targ = FOA(tab);
+
+		if(targ == null) return; 
+		EditorGUILayout.BeginHorizontal();
+		GUILayout.Label("Elements: " + targ.Length, EditorStyles.boldLabel);
+		EditorGUILayout.EndHorizontal();
+
+		EditorGUILayout.BeginHorizontal();
+		if(GUILayout.Button("<")) targ.Index --;
+		GUILayout.Label(targ.Current.Name +"", EditorStyles.boldLabel);
+		if(GUILayout.Button(">")) targ.Index++;
+		EditorGUILayout.Space();
+		targ.Current._Scale = EditorGUILayout.Vector3Field("Scale", targ.Current._Scale);
+		EditorGUILayout.EndHorizontal();
+
+		EditorGUILayout.Space();
+		EditorGUILayout.BeginHorizontal();
+		targ.Current._Position = EditorGUILayout.Vector3Field("Position", targ.Current._Position);
+		EditorGUILayout.Space();
+		targ.Current._Rotation = EditorGUILayout.Vector3Field("Rotation", targ.Current._Rotation);
+		EditorGUILayout.EndHorizontal();
+
+		CheckDifferences();
+		
+	}
+	public Transform FaceParent;
+	public FaceObj TargetGrand;
+
+	public void Generate()
+	{
+		if(TargetGrand != null) DestroyImmediate(TargetGrand.gameObject);
+		GameObject _base = Instantiate(Base.Current.Prefab);
+		_base.transform.SetParent(FaceParent.transform);
+
+		TargetGrand = _base.GetComponent<FaceObj>();
+
+		TargetGrand.CreateAnchor(0, Eye.Current);
+		TargetGrand.CreateAnchor(1, Brow.Current);
+		TargetGrand.CreateAnchor(2, Ear.Current);
+		TargetGrand.CreateAnchor(3, Jaw.Current);
+		TargetGrand.CreateAnchor(4, Hair.Current);
+
+		_base.name = "GRAND";
+	}
+
+	void CheckDifferences()
+	{
+		if(TargetGrand == null) return;
+
+		TargetGrand.GetInfo(0, Eye.Current);
+		TargetGrand.GetInfo(1, Brow.Current);
+		TargetGrand.GetInfo(2, Ear.Current);
+		TargetGrand.GetInfo(3, Jaw.Current);
+		TargetGrand.GetInfo(4, Hair.Current);
+
+	}
+
+	void OnEnable()
+	{
+		elements_loaded = false;
+		LoadElements();
+		FaceParent = GameObject.Find("FaceParent").transform;
+	}
+
+	bool elements_loaded = false;
+	void LoadElements()
+	{
+		List<Object> _base = new List<Object>();
+		List<FaceObjInfo> final = new List<FaceObjInfo>();
+		_base.AddRange(Resources.LoadAll("Objects/Base", typeof(Object)));
+		for(int i =0 ; i < _base.Count; i++)
+		{
+			GameObject g = _base[i] as GameObject;
+			if((g != null) && g.transform.root ==g.transform)
+			{
+				final.Add(new FaceObjInfo(i, g));
+			}
+		}
+		Base = new FaceObjInfoContainer(final.ToArray());
+
+		List<Object> _eye = new List<Object>();
+		final = new List<FaceObjInfo>();
+		_eye.AddRange(Resources.LoadAll("Objects/Eye", typeof(Object)));
+		for(int i =0 ; i < _eye.Count; i++)
+		{
+			GameObject g = _eye[i] as GameObject;
+			if((g != null) && g.transform.root ==g.transform)
+			{
+				final.Add(new FaceObjInfo(i, g));
+			}
+		}
+		Eye =new FaceObjInfoContainer(final.ToArray());
+
+		List<Object> _nose = new List<Object>();
+		final = new List<FaceObjInfo>();
+		_nose.AddRange(Resources.LoadAll("Objects/Nose", typeof(Object)));
+		for(int i =0 ; i < _nose.Count; i++)
+		{
+			GameObject g = _nose[i] as GameObject;
+			if((g != null) && g.transform.root ==g.transform)
+			{
+				final.Add(new FaceObjInfo(i, g));
+			}
+		}
+		Nose = new FaceObjInfoContainer(final.ToArray());
+
+		List<Object> _brow = new List<Object>();
+		final = new List<FaceObjInfo>();
+		_brow.AddRange(Resources.LoadAll("Objects/Brow", typeof(Object)));
+		for(int i =0 ; i < _brow.Count; i++)
+		{
+			GameObject g = _brow[i] as GameObject;
+			if((g != null) && g.transform.root ==g.transform)
+			{
+				final.Add(new FaceObjInfo(i, g));
+			}
+		}
+		Brow = new FaceObjInfoContainer(final.ToArray());
+
+		List<Object> _hair = new List<Object>();
+		final = new List<FaceObjInfo>();
+		_hair.AddRange(Resources.LoadAll("Objects/Hair", typeof(Object)));
+		for(int i =0 ; i < _hair.Count; i++)
+		{
+			GameObject g = _hair[i] as GameObject;
+			if((g != null) && g.transform.root ==g.transform)
+			{
+				final.Add(new FaceObjInfo(i, g));
+			}
+		}
+		Hair = new FaceObjInfoContainer(final.ToArray());
+
+		List<Object> _ear = new List<Object>();
+		final = new List<FaceObjInfo>();
+		_ear.AddRange(Resources.LoadAll("Objects/Ear", typeof(Object)));
+		for(int i =0 ; i < _ear.Count; i++)
+		{
+			GameObject g = _ear[i] as GameObject;
+			if((g != null) && g.transform.root ==g.transform)
+			{
+				final.Add(new FaceObjInfo(i, g));
+			}
+		}
+		Ear = new FaceObjInfoContainer(final.ToArray());
+
+		List<Object> _jaw = new List<Object>();
+		final = new List<FaceObjInfo>();
+		_jaw.AddRange(Resources.LoadAll("Objects/Jaw", typeof(Object)));
+		for(int i =0 ; i < _jaw.Count; i++)
+		{
+			GameObject g = _jaw[i] as GameObject;
+			if((g != null) && g.transform.root ==g.transform)
+			{
+				final.Add(new FaceObjInfo(i, g));
+			}
+		}
+		Jaw = new FaceObjInfoContainer(final.ToArray());
+
+		elements_loaded = true;
+	}
+
+
+}
---
+++ Assets/Generator/FaceObj.cs
@@ -0,0 +1,231 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class FaceObj : UIObj {
+
+	//public RectTransform [] Anchors;
+
+	public FaceObj FaceParent;
+	public bool GetSkinColor, GetHairColor;
+
+	private RectTransform _anchorpoint;
+	private FaceObj [] _obj;
+	private RectTransform trans;
+
+	public List<AnimTrigger> Anims = new List<AnimTrigger>();
+	
+
+	public void CheckAnims()
+	{
+		if(Anims != null)
+		{
+			for(int i = 0; i < Anims.Count; i++)
+			{
+				if(Anims[i].Update()) 
+				{
+					Anims[i].Trigger();
+				}
+					
+			}
+		}
+		if(Child != null)
+		{
+			for(int i = 0; i < Child.Length; i++)
+			{
+				if(Child[i] is FaceObj) (Child[i] as FaceObj).CheckAnims();
+			}
+		}
+	}
+
+	public void CreateAnchor(int num, FaceObj o)
+	{
+		if(_obj == null || _obj.Length == 0) _obj = new FaceObj[Child.Length];
+		_obj[num] = (FaceObj) Instantiate(o);
+		_obj[num].SetAnchorPoint(Child[num] as FaceObj);
+	}
+
+	public void CreateAnchor(int num, FaceObjInfo o)
+	{
+		if(_obj == null || _obj.Length == 0) _obj = new FaceObj[Child.Length];
+
+		if(_obj[num] != null) DestroyImmediate(_obj[num].gameObject);
+
+		GameObject f = Instantiate(o.Prefab);
+		FaceObj fin = f.GetComponent<FaceObj>();
+		f.transform.position = Child[num].transform.position + o._Position;
+		f.transform.Rotate(o._Rotation);
+		f.transform.localScale = o._Scale;
+
+		fin.Index = o.Index;
+
+		_obj[num] = fin;
+		_obj[num].SetAnchorPoint(Child[num] as FaceObj);
+	}
+
+
+	public void SetAnchorPoint(FaceObj t)
+	{
+		SetParent(t);
+
+		_anchorpoint = t.transform as RectTransform;
+
+		if(!trans) trans = this.transform as RectTransform;
+
+		trans.anchorMax = Vector2.one;//_anchorpoint.anchorMax;
+		trans.anchorMin = Vector2.zero;//_anchorpoint.anchorMin;
+		trans.anchoredPosition = Vector2.zero;//_anchorpoint.anchoredPosition;
+		trans.localScale = Vector3.one;
+		trans.sizeDelta = Vector2.zero;//_anchorpoint.sizeDelta;
+	}
+
+	public FaceObj GetObj(int num) {return _obj[num];}
+
+	public void GetInfo(int num, FaceObjInfo f)
+	{
+		if(_obj[num].Index != f.Index) CreateAnchor(num, f);
+
+		_obj[num].transform.position = Child[num].transform.position + f._Position;
+		_obj[num].transform.rotation = Quaternion.Euler(f._Rotation);
+		_obj[num].transform.localScale = f._Scale;
+	}
+
+	private Color	_skincol;
+	public Color SkinCol
+	{get{return _skincol;}}
+	public void SetSkinColor(Color c)
+	{
+		_skincol = c;
+	}
+
+	private Color	_haircol;
+	public Color HairCol
+	{get{return _haircol;}}
+	public void SetHairColor(Color c)
+	{
+		_haircol = c;
+	}
+
+	public override void SetParent(UIObj p)
+	{
+		ParentObj = p;
+		transform.SetParent(p.transform);
+		p.AddChild(this);
+
+		if(p is FaceObj)
+		{
+			FaceObj f = p as FaceObj;
+			if(f.FaceParent) 
+			{
+				SetFaceParent(f.FaceParent);
+			}
+		}
+	}
+
+	public void SetFaceParent(FaceObj f)
+	{
+		FaceParent = f;
+		if(GetSkinColor && _Image) _Image.color = FaceParent.SkinCol;
+		if(GetHairColor && _Image) _Image.color = FaceParent.HairCol;
+
+		for(int i = 0; i < Child.Length; i++)
+		{
+			if(Child[i] is FaceObj)
+			{
+				(Child[i] as FaceObj).SetFaceParent(FaceParent);
+			}
+		}
+	}
+
+	public void AddAnimTrigger(string title, Vector2 times, params Animator [] anims)
+	{
+		AnimTrigger a = new AnimTrigger(title, times, anims);
+		Anims.Add(a);
+	}
+
+}
+	
+[System.Serializable]
+public class AnimTrigger
+{
+	public string Title;
+	public Vector2 TimeConstants;
+	public float TimeActual, Current;
+	public Animator [] Anims;
+
+	public AnimTrigger(string t, Vector2 c, params Animator [] a)
+	{
+		Title = t;
+		TimeConstants = c;
+		Anims = a;
+		Create();
+
+	}
+	public void Create()
+	{
+		TimeActual = Random.Range(TimeConstants.x, TimeConstants.y);
+	}
+
+	public bool Update()
+	{
+		Current += Time.deltaTime;
+		if(Current > TimeActual)
+		{
+			Create();
+			Current = 0.0F;
+			return true;
+		}
+		return false;
+	}
+
+	public void Trigger()
+	{
+		for(int i = 0; i < Anims.Length; i++)
+		{
+			Anims[i].SetTrigger(Title);
+		}
+	}
+}
+
+
+public class FaceObjInfoContainer
+{
+	private int _index = 0;
+	public int Index{
+		get{return _index;}
+		set{_index = value;
+			if(_index < 0) _index = Objs.Length-1; 
+			else if(_index > Objs.Length-1) _index = 0;
+			}
+	}
+	public FaceObjInfo [] Objs;
+	public FaceObjInfo this[int num]{get{return Objs[num];}}
+	public int Length{get{return Objs.Length;}}
+	public FaceObjInfo Current{get{return Objs[Index];}}
+	public FaceObjInfoContainer(FaceObjInfo [] f)
+	{
+		Objs = f;
+		_index = 0;
+	}
+}
+
+public class FaceObjInfo
+{
+	public int Index;
+	public GameObject Prefab;
+
+	public Vector3 _Position;
+	public Vector3 _Rotation;
+	public Vector3 _Scale;
+
+	public FaceObjInfo(int ind, GameObject p)
+	{
+		Index = ind;
+		Prefab = p;
+		_Position = Vector3.zero;
+		_Rotation = Vector3.zero;
+		_Scale = Vector3.one;
+	}
+
+	public string Name{get{return Prefab.name;}}
+}
